bdcb8600c35dfbfd81a7b4a0156d1dd54aac9662
trsif <pepefava1@gmail.com>
2021-05-18 10:26:42 -0400

    refacterd for better use

--- java/helloworld/src/com/loop/runner.java
+++ java/helloworld/src/com/loop/runner.java
@@ -1,16 +1,19 @@
 package com.loop;
 
 public class runner {
-		public static int num[] = {1,2,3,4,5,6,7,8,9,10};
+		public static int num[] = new int[10];
 	public static void main(String[] args) {
 	
 		for(int i = 0 ; i < num.length; i++) {
 //			i = i+1 ;
-			System.out.println(num[i]);
+			num[i]= i+1;
+			System.out.print(num[i]+ " ");
 			
 		}
+		System.out.println(" ");
 		for (int j = 0 ; j < num.length; j++) {
-			System.out.println(num[j] * 10);
+			num[j] *= 10;
+			System.out.print(num[j]+ " ");
 		}
 	}
 
