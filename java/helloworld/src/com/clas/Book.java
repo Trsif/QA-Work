1b06e579969a25a4de9dc2ce7689d0fab64b6b54
trsif <pepefava1@gmail.com>
2021-05-18 17:24:04 -0400

    added classes for person and book

---
+++ java/helloworld/src/com/clas/Book.java
@@ -0,0 +1,15 @@
+package com.clas;
+
+public class Book {
+	public String title;
+	public String author;
+	public String genre;
+	public int pages;
+	public String description;
+
+	public Book(String title, int pages, String author) {
+		this.title = title;
+		this.pages = pages;
+		this.author = author;
+	}
+}
--- java/helloworld/src/com/string/Runner.java
+++ java/helloworld/src/com/string/Runner.java
@@ -8,12 +8,49 @@ public class Runner {
 		System.out.print(str2.toUpperCase()+", " +str1.toUpperCase());
 		System.out.println(" ");
 		System.out.println(stringCount(str2));
+		System.out.println(" ");
+		stringWord(str2);
+		stringWordR(str2);
 	}
 	
 	public static int stringCount(String string) {
+		// Method 1 - When given a String, count and return how many words there are in that String.
 		return string.length();
 	}
-	public static void stringWord(String string) {
+	public static void  stringWord(String string) {
+		// Method 2 - When given a String, print out this String in a vertical fashion, each word on a different line.
+		int count = 0 ;
+		string = string + " ";
+		System.out.println(string);
+		System.out.println(string.length());
+		for(int i = 0 ; i < string.length(); i++) {
+			
+			String singleChar = string.substring(i,i+1);
+			if(singleChar.equals(" ")) {
+				System.out.println(string.substring(i-count,i));
+				count = 0;
+			}else {
+				count ++;
+			}
+		}
+		}
+		public static void  stringWordR(String string) {
+			// Method 2 - When given a String, print out this String in a vertical fashion, each word on a different line.
+			int count = 0 ;
+			string = string + " ";
+			System.out.println(string);
+			System.out.println(string.length());
+			for(int i = 0 ; i < string.length(); i++) {
+				
+				String singleChar = string.substring(i,i+1);
+				if(singleChar.equals(" ")) {
+					System.out.println(string.substring(i+count,i));
+					count = 0;
+				}else {
+					count ++;
+				}
+			}
+//		return string.substring(0, 6);
 		
 	}
 }
